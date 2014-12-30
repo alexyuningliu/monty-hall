42c6f935ca4f03ce8d0fbcb166a9d59163b1b944
Alex Liu <yuningalexliu@gmail.com>
2014-12-29 19:29:46 -0500

    added jQuery locally and also reached beta

--- js/engine.js
+++ js/engine.js
@@ -12,7 +12,10 @@ function Door(itemName, position) {
 }
 
 function Stage(door1, door2, door3) {
-	this.doors = [].push(door1).push(door2).push(door3);
+	this.doors = [];
+	this.doors.push(door1);
+	this.doors.push(door2);
+	this.doors.push(door3);
 }
 
 function Outcome() {
@@ -22,16 +25,14 @@ function Outcome() {
 
 // Prototypes
 
-Player.prototype.firstPick = function() {
+Player.prototype.firstPick = function(stage) {
 
- // TODO v
-
-	var pick1Index = Math.shuffle(stage.doors.length).floor()
+	var pick1Index = Math.floor(Math.random() * stage.doors.length);
 	this.pick1 = stage.doors[pick1Index];
 	this.pick1.currentlySelected = true;
 }
 
-Player.prototype.secondPick = function() {
+Player.prototype.secondPick = function(stage) {
 	if (this.strategy === "always-switch") {
 		for (var i = 0; i < stage.doors.length; i++) {
 			if ((stage.doors[i].itemName !== "prize") && (stage.doors[i].revealed === false)) {
@@ -47,32 +48,24 @@ Player.prototype.secondPick = function() {
 
 Stage.prototype.revealGoat = function() {
 	for (var i = 0; i < this.doors.length; i++) {
-		if ((this.doors[i].itemName === ("goat1" || "goat2")) 
+		if (((this.doors[i].itemName == "goat1") || (this.doors[i].itemName == "goat2")) 
 			&& (this.doors[i].currentlySelected === false)) {
 			this.doors[i].revealed = true;
+			break;
 		} else {
-			console.log("Error! No doors with unselected goats found!")
+			// console.log("Not a goat!");
 		}
 	}
 }
 
-Stage.prototype.revealGoat = function() {
-	for (var i = 0; i < this.doors.length; i++) {
-		if ((this.doors[i].itemName === ("goat1" || "goat2")) 
-			&& (this.doors[i].currentlySelected === false)) {
-			this.doors[i].revealed = true;
-		} else {
-			console.log("Error! No doors with unselected goats found!")
-		}
-	}
-}
-
-Stage.prototype.determineOutcome = function() {
+Stage.prototype.determineOutcome = function(outcome) {
 	for (var i = 0; i < this.doors.length; i++) {
 		if ((this.doors[i].itemName === "prize") && (this.doors[i].currentlySelected === true)) {
 			outcome.wins++;
-		} else {
+			break;
+		} else if (i === (this.doors.length - 1)) {
 			outcome.losses++;
+			break;
 		}
 	}
 }
@@ -84,61 +77,57 @@ function shuffle(o){ //v1.0
     return o;
 };
 
-function execute() {
-	
-	// Setup
-
-	var originalItemsArray = ["prize","goat1","goat2"];
-	var itemsArray = shuffle(originalItemsArray);
-
-	var door1 = new Door(itemsArray.pop(), 1);
-	var door2 = new Door(itemsArray.pop(), 2);
-	var door3 = new Door(itemsArray.pop(), 3);
-
-	var stage = new Stage(door1, door2, door3);
-
-	var outcome = new Outcome();
-
-	// Execute
-
-	player.firstPick();
-
-	stage.revealGoat();
-
-	player.secondPick();
-
-	stage.determineOutcome();
-
-	console.log("There were this many wins: " + outcome.wins);
-	console.log("There were this many losses: "+ outcome.losses);
-
-	// Teardown
-}
-
-
-
 // Run simulator
 
-function initSetup(strategy) {
-	player = new Player(strategy);
-	outcome = new Outcome();
-}
+// function initSetup(strategy) {
+// 	player = new Player(strategy);
+// 	outcome = new Outcome();
+// }
 
 function runSimulator(strategy) {
 	var limit = 1;
 	for (var i = 0; i <= limit; i++) {
-		execute();
+		// Creating more permanent objects
+
+		var player = new Player(strategy);
+		var outcome = new Outcome();	
+				
+		// Setup
+
+		var originalItemsArray = ["prize","goat1","goat2"];
+		var itemsArray = shuffle(originalItemsArray);
+
+		var door1 = new Door(itemsArray.pop(), 1);
+		var door2 = new Door(itemsArray.pop(), 2);
+		var door3 = new Door(itemsArray.pop(), 3);
+
+		var stage = new Stage(door1, door2, door3);
+
+		// Execute
+
+		player.firstPick(stage);
+
+		stage.revealGoat();
+
+		player.secondPick(stage);
+
+		stage.determineOutcome(outcome);
+
+		console.log("There were this many wins: " + outcome.wins);
+		console.log("There were this many losses: "+ outcome.losses);
+
+		// Teardown
 	}
 }
 
 // Listening for events on DOM
 
 $("#always-switch").on("click", function() {
-	initSetup("always-switch");
+	// initSetup("always-switch");
 	runSimulator("always-switch");
 })
 
 $("#never-switch").on("click", function() {
-	initSetup("never-switch");
+	// initSetup("never-switch");
 	runSimulator("never-switch");
 })
